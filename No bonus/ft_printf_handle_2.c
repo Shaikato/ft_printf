00a6a2c7b67f8a2bfc65aab4990db9e67cebbb31
Ryan Andre <randre@student.s19.be>
2023-10-07 01:43:02 +0200

    p add

--- No bonus/ft_printf_handle_2.c
+++ No bonus/ft_printf_handle_2.c
@@ -6,7 +6,7 @@
 /*   By: randre <randre@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 08:45:22 by randre            #+#    #+#             */
-/*   Updated: 2023/10/07 00:59:25 by randre           ###   ########.fr       */
+/*   Updated: 2023/10/07 01:42:38 by randre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	ft_phandle(va_list args)
 		write(1, "(nil)", 5);
 	else
 	{
+		write(1, "0x", 2);
 		nbr = (intptr_t)p;
 		ft_hexa(nbr);
 	}
